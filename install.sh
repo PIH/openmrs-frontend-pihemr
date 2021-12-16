3617370125fc7b758cf2b7c7d9ef3b73e14bf9e7
Brandon Istenes <bistenes@gmail.com>
2021-12-16 15:40:20 -0800

    Add symlink command

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 usage () {
     echo -e "Usage: install.sh [SERVER]\n"
     echo -e "Installs the frontend to SERVER, where a server is the name of an OpenMRS SDK instance at path '~/openmrs/[SERVER]'\n"
+    echo -e "Also symlinks the frontend config from the configuration directory.\n"
     echo -e "Example: ./install.sh mirebalais\n"
 }
 
@@ -15,3 +16,4 @@ fi
 mvn clean compile
 rm -r ~/openmrs/$1/frontend
 cp -r target/dist ~/openmrs/$1/frontend
+ln -s ~/openmrs/$1/configuration/frontend ~/openmrs/$1/frontend/site
